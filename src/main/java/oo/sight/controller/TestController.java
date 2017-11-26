941586685cd3b7a07bde3e1756c4845eca2eb3da
陈嘉源 <jiayuan.chen@mljr.com>
2017-11-26 22:09:47 +0800

    添加日志打印

--- src/main/java/oo/sight/controller/TestController.java
+++ src/main/java/oo/sight/controller/TestController.java
@@ -1,6 +1,16 @@
 package oo.sight.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/sight/test")
 public class TestController {
-
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(){
+		System.out.println("hello....");
+		return "hello";
+	}
 }
